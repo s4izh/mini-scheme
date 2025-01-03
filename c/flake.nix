040c249fa677edf1f5dc71cc4c03c2bcad65a531
s4izh <sergiosanz234@gmail.com>
2025-01-03 15:22:12 +0100

    feat(c): add flake.nix for building

---
+++ c/flake.nix
@@ -0,0 +1,54 @@
+{
+  description = "A Nix flake using a separate shell.nix";
+
+  inputs = {
+    nixpkgs = {
+      url = "github:nixos/nixpkgs/nixos-unstable";
+    };
+    flake-utils = {
+      url = "github:numtide/flake-utils";
+    };
+  };
+  outputs =
+    { nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      {
+        packages = rec {
+          mscm = pkgs.stdenv.mkDerivation {
+            pname = "mscm";
+            version = "0.1.0";
+            src = ./.;
+
+            nativeBuildInputs = [
+              pkgs.gcc
+            ];
+
+            buildInputs = [
+              pkgs.readline
+            ];
+
+            buildPhase = ''
+              make
+            '';
+
+            installPhase = ''
+              mkdir -p $out/bin
+              cp build/mscm $out/bin
+            '';
+
+            meta = with pkgs.lib; {
+              description = "a (m)ini (sc)he(m)e interpreter";
+              license = licenses.mit;
+              platforms = platforms.all;
+            };
+          };
+          default = mscm;
+        };
+        # devShell = import ./shell.nix { inherit pkgs; };
+      }
+    );
+}
